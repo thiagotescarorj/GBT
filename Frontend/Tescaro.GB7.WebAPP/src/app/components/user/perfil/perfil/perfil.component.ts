43ed8f76fce817417a9ccf0fd4216a5b9101731b
thiagotescarorj <thiago.tescaro@outlook.com>
2023-04-29 18:28:41 -0300

    ok

--- Frontend/Tescaro.GB7.WebAPP/src/app/components/user/perfil/perfil/perfil.component.ts
+++ Frontend/Tescaro.GB7.WebAPP/src/app/components/user/perfil/perfil/perfil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidadorCampos } from 'src/app/helpers/ValidadorCampos';
 
 @Component({
   selector: 'app-perfil',
@@ -6,9 +8,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./perfil.component.css']
 })
 export class PerfilComponent implements OnInit {
-  constructor() { }
 
-  ngOnInit() {
+  perfilForm!: FormGroup;
+
+  constructor(private fb: FormBuilder){}
+
+  formOptions: AbstractControlOptions = {
+    validators: ValidadorCampos.ValidarCampos('senha', 'confirmarSenha')
+  };
+
+    ngOnInit(): void {
+      this.perfilForm = this.fb.group({
+        primeiroNome: ['', Validators.required],
+        ultimoNome: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        senha: ['', [Validators.required, Validators.minLength(6)]],
+        confirmarSenha: ['', Validators.required]
+      }, this.formOptions);
+  }
+
+  get primeiroNome(){
+    return this.perfilForm.get('primeiroNome')!;
+  }
+
+  get ultimoNome(){
+    return this.perfilForm.get('ultimoNome')!
+  }
+
+
+  get email(){
+    return this.perfilForm.get('email')!
+  }
+
+  get senha(){
+    return this.perfilForm.get('senha')!
+  }
+
+  get confirmarSenha(){
+    return this.perfilForm.get('confirmarSenha')!
+  }
+
+  submit(){
+    if(this.perfilForm.invalid){
+      return;
+    }
+    console.log('Criado Formulário')
+  }
+
+   resetForm(){
+    this.perfilForm.reset();
   }
 
 }
+
