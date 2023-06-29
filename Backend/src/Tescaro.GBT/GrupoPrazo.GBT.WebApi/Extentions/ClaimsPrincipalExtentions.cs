3c3051cbf28a6473d2b88cb784905377c29fe1ad
thiagotescarorj <thiago.tescaro@outlook.com>
2023-06-29 19:37:34 -0300

    teste vs
    teste vs

--- Backend/src/Tescaro.GBT/GrupoPrazo.GBT.WebApi/Extentions/ClaimsPrincipalExtentions.cs
+++ Backend/src/Tescaro.GBT/GrupoPrazo.GBT.WebApi/Extentions/ClaimsPrincipalExtentions.cs
@@ -6,7 +6,7 @@ namespace Tescaro.GBT.API.Extentions
     {
         public static string GetEmail(this ClaimsPrincipal user)
         {
-            return user.FindFirst(ClaimTypes.Email)?.Value;
+            return user.FindFirst(ClaimTypes.Name)?.Value;
         }
 
         public static long GetUserId(this ClaimsPrincipal user)
---
+++ Frontend/Tescaro.GB7/Tescaro.GB7.WebAPP/src/app/app.component.spec.ts
@@ -0,0 +1,27 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    declarations: [AppComponent]
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'Tescaro.GB7.WebAPP'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('Tescaro.GB7.WebAPP');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Tescaro.GB7.WebAPP app is running!');
+  });
+});
